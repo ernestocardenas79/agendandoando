36227eda43cfd9780f3722960d2b5e4186440fe8
Ernesto Cardenas LLanes <ernesto.cardenas79@gmail.com>
2022-05-02 16:08:04 -0600

    comenzando diseñando la pantalla de login

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pgs-login',
+  styleUrls: ['./login.component.css'],
   template: `
-    <section>
-      login works!
-      <h3 routerLink="/">Entrar</h3>
-    </section>
+    <form (submit)="loginHandler($event)">
+      <h3>Ingresar</h3>
+      <label for="user">User</label>
+      <input type="text" id="user" />
+      <label for="pasword">Password</label>
+      <input type="password" id="password" />
+      <button type="button">Cancelar</button>
+      <button type="submit">Ingresar</button>
+    </form>
   `,
-  styles: [],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  loginHandler(event: Event) {
+    console.log('eventa');
+    event.preventDefault();
+    this.router.navigate(['/']);
+  }
 }
--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [FormsModule, RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class SecurityRoutingModule {}
