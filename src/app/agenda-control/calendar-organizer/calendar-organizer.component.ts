ea90363a464f7ad259c88b6e5dcda96be17cba78
Ernesto Cardenas LLanes <ernesto.cardenas79@gmail.com>
2022-05-12 07:39:24 -0600

    #6 ajusteas al layout mostrando el calendar organizer

--- src/app/agenda-control/calendar-organizer/calendar-organizer.component.ts
+++ src/app/agenda-control/calendar-organizer/calendar-organizer.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { getWeek } from 'date-fns';
+import { getMonth, getWeek } from 'date-fns';
 
 @Component({
-  template: ` <section>calendar-organizer works! {{ weekId }}</section> `,
+  template: ` <section>
+    <h2>Citas de {{ month }}</h2>
+    <section class="days-container">
+      <pgs-day></pgs-day>
+      <pgs-day></pgs-day>
+      <pgs-day></pgs-day>
+      <pgs-day></pgs-day>
+      <pgs-day></pgs-day>
+    </section>
+  </section>`,
   styleUrls: ['./calendar-organizer.component.css'],
 })
 export class CalendarOrganizerComponent {
   weekId: number;
+  month: string;
   constructor() {
     this.weekId = getWeek(new Date());
+    this.month = getMonth(new Date()).toString();
   }
 }
--- src/app/agenda-control/day/day.component.ts
+++ src/app/agenda-control/day/day.component.ts
@@ -8,8 +8,77 @@ import { Appoiment } from 'src/app/core/models/appoiment';
 
 @Component({
   selector: 'pgs-day',
-  template: ` <p>day works!</p> `,
-  styles: [],
+  template: ` <section>Lunes</section>
+    <article>
+      <h2>7</h2>
+      <aside>
+        <span>5:00</span>
+        <span>5:30</span>
+        <span>6:00</span>
+        <span>6:30</span>
+        <span>6:30</span>
+        <span>7:00</span>
+        <span>7:30</span>
+        <span>8:00</span>
+        <span>6:30</span>
+        <span>7:00</span>
+        <span>7:30</span>
+        <span>8:00</span>
+      </aside>
+    </article>`,
+  styles: [
+    `
+      :host {
+        display: block;
+        width: 19%;
+        height: 90%;
+        background-color: red;
+        font-size: x-large;
+      }
+
+      article {
+        display: flex;
+        background-color: gray;
+        height: calc(90% - 7px);
+      }
+
+      article h2 {
+        display: inline-block;
+        background-color: yellow;
+        width: 60%;
+        border-radius: 0 0 0 5px;
+        font-size: 350%;
+        text-align: center;
+        margin: auto;
+      }
+
+      article aside {
+        overflow-x: hidden;
+        overflow-y: auto;
+        max-height: calc(100% - 5px);
+        width: 40%;
+      }
+
+      article aside span {
+        display: inline-block;
+        font-size: 70%;
+        height: 30px;
+        max-height: 50px;
+        padding-top: 5px;
+        text-align: center;
+        vertical-align: middle;
+        width: 80px;
+      }
+
+      section {
+        background-color: blue;
+        color: white;
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 5px 5px 0 0;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DayComponent {
